cabaa24ab99a6cb1ea026e4aa033bf2faec69e43
Tanmay Agrawal <tanmayagrawal2004@gmail.com>
2026-03-27 12:16:36 -0700

    add gitignore, remove env and metadata files

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     DATABASE_URL: str = "sqlite:///./taskdb.sqlite"
-    SECRET_KEY: str = "supersecretkey-change-in-production"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 480
 
